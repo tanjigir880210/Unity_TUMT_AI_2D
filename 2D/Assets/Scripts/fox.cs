cfabfee9305c901973587489201ab8f7efd81f8c
tanjigir880210 <55218583+tanjigir880210@users.noreply.github.com>
2019-09-26 12:06:25 +0800

    C#_20190926

---
+++ 2D/Assets/Scripts/fox.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class fox : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("哈囉 薩諾斯彈指");
+        Debug.Log("薩諾斯:宇宙死一半了");
+        Debug.Log("薩諾斯:我的使命完成");
+        Debug.Log("約翰維克:有人看到我的狗嗎?");
+        Debug.Log("薩諾斯:幹!!!");
+        Debug.Log(".........");
+        Debug.Log(777);
+        Debug.Log(1.5f);
+    }
+}
