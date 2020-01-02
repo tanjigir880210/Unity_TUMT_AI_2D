6086d0895932394554697b42338181a53eeb3d98
tanjigir880210 <55218583+tanjigir880210@users.noreply.github.com>
2020-01-02 10:01:03 +0800

    改

---
+++ 2D/Assets/Assets/script/Enemy.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("移動速度"), Range(0, 100)]
+    public float speed = 1.5f;
+
+    [Header("傷害"), Range(0, 100)]
+    public float damage = 35;
+
+    public Transform checkpoint;
+
+    private Rigidbody2D r2d;
+
+
+    private void Start()
+    {
+        r2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawRay(checkpoint.position, -checkpoint.up * 2);
+    }
+
+
+    ///<summary>
+    ///隨機移動
+    ///</summary>
+    private void Move()
+    {
+        //r2d.AddForce(new Vector2(-speed, 0)); 世界座標
+        r2d.AddForce(-transform.right * speed); // 區域座標 2D transform.right 右邊 transform.up 上方
+
+        //碰撞資訊 = 物理.射線碰撞
+        RaycastHit2D hit = Physics2D.Raycast(checkpoint.position, -checkpoint.up, 1.5f, 1 << 8);
+
+        //print(hit.collider.gameObject);
+
+        if (hit == false)
+        {
+            transform.eulerAngles += new Vector3(0, 180, 0);
+
+        }
+
+
+
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.name == "狐狸")
+        {
+            Track(collision.transform.position);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "狐狸")
+        {
+            collision.gameObject.GetComponent<Fox>().Damage(damage);
+
+        }
+    }
+
+
+
+
+
+    /// <summary>
+    /// 追蹤玩家
+    /// </summary>
+    /// <param name="target">玩家座標</param>
+    private void Track(Vector3 target)
+    {
+        if (target.x < transform.position.x)
+        {
+            transform.eulerAngles = Vector3.zero;  // new Vector3(0,0,0)
+        }
+        else
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+
+
+    }
+
+
+
+
+
+
+}
---
+++ 2D/Assets/Assets/script/LearnArrayFor.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;   // 引用 系統.集合 API
+
+public class LearnArrayFor : MonoBehaviour
+{
+    public string say = "HI!!!";
+
+    // 類型 陣列[] = new 類型[數量]
+    public float[] values = new float[7];
+    // { 陣列資料 }
+    public int[] scores = { 100, 80, 90, 30, 40 };
+
+    public Color[] colors = new Color[5];
+
+    private void Start()
+    {
+        // 陣列編號從 0 開始
+        print("第三個字：" + say[2]);
+        // 取得陣列長度：陣列.Length
+        print("陣列的長度：" + say.Length);
+
+        // 存放陣列
+        scores[2] = 60;
+        // 取得陣列
+        print(scores[2]);
+
+        for (int i = 1; i <= 10; i++)
+        {
+            //print("數字：" + i);
+        }
+
+        for (int i = 0; i < scores.Length; i++)
+        {
+            //print("分數陣列：" + scores[i]);
+        }
+
+       // StartCoroutine(Test()); // 啟動協程
+        StartCoroutine(printer());
+    }
+
+    // 協同程序
+    private IEnumerator Test()
+    {
+        print("開始!");
+        yield return new WaitForSeconds(1);
+        print("<color=black>1秒鐘測試~</color>");
+        yield return new WaitForSeconds(2);
+        print("<color=black>2秒鐘測試~</color>");
+
+    }
+
+    private IEnumerator printer()
+    {
+        for (int i = 0; i < say.Length; i++)
+        {
+            print(say[i]);
+            yield return new WaitForSeconds(0.3f);
+        }
+        
+    }
+}
